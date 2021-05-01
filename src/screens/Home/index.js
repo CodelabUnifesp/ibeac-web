4a476aeda55aeda883fa071d6eb576bef788715d
DanielSalis <danielsalis98@gmail.com>
2021-05-01 20:09:49 -0300

    fix: corrigindo pasta de componentes duplicada e corrigindo erro do babel

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {BodyContainer} from '../../Components/Body';
-import Header from '../../Components/Header';
+import BodyContainer from '../../components/Body';
+import Header from '../../components/Header';
 
 function Home(props) {
   return (
