a54f061a1a3d59fda3057d48f0fa065347761716
DanielSalis <danielsalis98@gmail.com>
2021-06-13 15:10:45 -0300

    fix: mudando nome da página

--- src/screens/RegisterUser/index.js
+++ src/screens/RegisterUser/index.js
@@ -99,7 +99,7 @@ const RegisterUser = (...props) => {
   return (
     <S.Wrapper px={{base: 0, lg: 6}}>
       <Text color="#2f7384" fontSize="2xl" fontWeight={600} marginBottom={4}>
-        Formulário Socioeconômico
+        Adicionar usuário
       </Text>
       <Box
         borderRadius={10}
