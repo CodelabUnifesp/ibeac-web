46ecdb344fc18b16394ab7405b3eea12655bfeee
CodelabUnifesp <39316258+CodelabUnifesp@users.noreply.github.com>
2021-06-28 16:02:32 -0300

    Update api.js
    Fixing prettier error

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.PLASMEDIS_API || 'https://ibeac-plasmedis.herokuapp.com/',
+  baseURL:
+    process.env.PLASMEDIS_API || 'https://ibeac-plasmedis.herokuapp.com/',
 });
 
 export default api;
