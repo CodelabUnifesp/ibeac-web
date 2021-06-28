ed43ed916726f1903fa2f22a0fb1601587832c16
CodelabUnifesp <39316258+CodelabUnifesp@users.noreply.github.com>
2021-06-28 15:56:42 -0300

    Update api.js
    Adicionando variavel de ambiente PLASMEDIS_API com referencia à api do plasmedis

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ibeac-plasmedis.herokuapp.com/',
+  baseURL: process.env.PLASMEDIS_API || 'https://ibeac-plasmedis.herokuapp.com/',
 });
 
 export default api;
