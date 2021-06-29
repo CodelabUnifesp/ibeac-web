5a690c5e3bb3005c5054d8bf3b451b8cf2ae4e29
Danilo Alexandre <danilo.salexan@gmail.com>
2021-06-29 17:50:54 -0300

    arrumando envvar para api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
-    process.env.PLASMEDIS_API || 'https://ibeac-plasmedis.herokuapp.com/',
+    process.env.REACT_APP_PLASMEDIS_API ||
+    'https://ibeac-plasmedis.herokuapp.com/',
 });
 
 export default api;
