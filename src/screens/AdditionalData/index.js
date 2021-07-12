cf2994c6d4c937f08de26afe35e2ff9653e9d2a8
DanielSalis <danielsalis98@gmail.com>
2021-07-11 22:12:02 -0300

    preparando tela para receber dados da api

--- src/screens/AdditionalData/index.js
+++ src/screens/AdditionalData/index.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text} from '@chakra-ui/react';
 import * as S from './styles';
 import questions from './questions';
 import FormQuestions from '../../components/elements/FormQuestions';
+import {getById as userGetById} from '../../domain/usuarios';
 
-const Form = (...props) => (
-  <S.Wrapper px={{base: 0, lg: 6}}>
-    <Text color="#2f7384" fontSize="2xl" fontWeight={600} marginBottom={4}>
-      Complemento de Dados
-    </Text>
-    <FormQuestions buttonName="Salvar" questions={questions} />
-  </S.Wrapper>
-);
+const Form = (...props) => {
+  const [currentUser] = useState(JSON.parse(localStorage.getItem('userData')));
+  const [aditionalData, setAditionalData] = useState(null);
+  const [override, setOverride] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      debugger;
+      const response = await userGetById(currentUser.id);
+      await setAditionalData(response);
+      await setOverride(false);
+    }
+    fetchData();
+  }, [currentUser]);
+
+  return (
+    <S.Wrapper px={{base: 0, lg: 6}}>
+      <Text color="#2f7384" fontSize="2xl" fontWeight={600} marginBottom={4}>
+        Complemento de Dados
+      </Text>
+      {aditionalData && (
+        <FormQuestions
+          buttonName="Salvar"
+          questions={questions}
+          userAdicionalData={aditionalData}
+          override={override}
+        />
+      )}
+    </S.Wrapper>
+  );
+};
 
 export default Form;
