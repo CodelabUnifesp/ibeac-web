5e3575801594b274936e049ca47537af1be761c5
DanielSalis <danielsalis98@gmail.com>
2021-07-26 14:05:07 -0300

    adaptando update e get

--- src/domain/formSocio/getById.js
+++ src/domain/formSocio/getById.js
@@ -1,10 +1,14 @@
 import api from '../../services/api';
 
-export default async function getById(userId) {
+export default async function getById(userId, token) {
   try {
     debugger;
-    const response = await api.get(`/form_socio/${userId}`);
-    return response.data.user;
+    const response = await api.get(`/form_socio/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.users[0];
   } catch (e) {
     alert('Erro ao encontar informações');
     return null;
--- src/domain/formSocio/updateById.js
+++ src/domain/formSocio/updateById.js
@@ -1,6 +1,6 @@
 import api from '../../services/api';
 
-export default async function updateById(userId, objectToSend) {
+export default async function updateById(userId, token, objectToSend) {
   try {
     // nome_rep_familia=data['nome_rep_familia'],
     // pessoa=data['pessoa'],
@@ -11,20 +11,38 @@ export default async function updateById(userId, objectToSend) {
     // qtd_amamentando=data['qtd_amamentando'],
     // qtd_criancas_deficiencia=data['qtd_criancas_deficiencia'],
     // qtd_gestantes=data['qtd_gestantes']
-
     debugger;
+
+    if (objectToSend) {
+      if (objectToSend[5] == 'Sim') {
+        objectToSend[5] = true;
+      } else if (objectToSend[5] == 'Não') {
+        objectToSend[5] = false;
+      }
+
+      if (objectToSend[6] == 'Sim') {
+        objectToSend[6] = true;
+      } else if (objectToSend[6] == 'Não') {
+        objectToSend[6] = false;
+      }
+    }
+
     const obj = {
       nome_rep_familia: objectToSend[1],
-      pessoa: objectToSend[2],
-      qtd_pessoas_familia: objectToSend[3],
-      pessoa_amamenta: objectToSend[4],
-      qtd_criancas: objectToSend[5],
+      qtd_pessoas_familia: objectToSend[2],
+      qtd_criancas: objectToSend[3],
+      qtd_gestantes: objectToSend[4],
+      pessoa_amamenta: objectToSend[5],
       gestante: objectToSend[6],
-      qtd_criancas_deficiencia: objectToSend[7],
-      qtd_gestantes: objectToSend[8],
+      qtd_amamentando: objectToSend[7],
+      qtd_criancas_deficiencia: objectToSend[8],
     };
 
-    await api.put(`form_socio/${userId}`, obj);
+    await api.post(`form_socio_by_user_id/${userId}`, obj, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     alert('Informações Atualizadas!');
   } catch (e) {
     alert('Erro ao submeter informações');
