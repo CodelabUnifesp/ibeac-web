af575c90436262445946e9a68b203499cd3a7803
Milena Siqueira <milematoss@hotmail.com>
2021-09-02 23:16:26 -0300

    adiciona modal para exibir mensagem de feedback

--- src/components/elements/Postagem/index.js
+++ src/components/elements/Postagem/index.js
@@ -3,9 +3,22 @@ import PropTypes from 'prop-types';
 
 import {Stack, Box, Text, Flex} from '@chakra-ui/layout';
 import {Avatar} from '@chakra-ui/avatar';
-import {IconButton, Input, Spinner} from '@chakra-ui/react';
+import {
+  IconButton,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Spinner,
+  useDisclosure,
+} from '@chakra-ui/react';
 import {MdSend, MdVerifiedUser} from 'react-icons/md';
 
+import {Button} from '@chakra-ui/button';
 import Icon from '@chakra-ui/icon';
 
 import {get, isEmpty, isNull} from 'lodash';
@@ -28,6 +41,11 @@ const Postagem = ({
 
   const [verifyingPost, setVerifyingPost] = useState(false);
 
+  const {isOpen, onOpen, onClose} = useDisclosure();
+
+  const [messageHeader, setMessageHeader] = useState('');
+  const [messageBody, setMessageBody] = useState('');
+
   useEffect(() => {
     if (openComments && item?.id) {
       if (item?.comments > 0) setLoadingComments(true);
@@ -43,10 +61,13 @@ const Postagem = ({
     if (verifyingPost && item?.id && onAddSelo) {
       onAddSelo(item.id).then((success) => {
         if (success) {
-          alert('A postagem foi verificada com sucesso!');
+          setMessageHeader('Sucesso!');
+          setMessageBody('A postagem foi verificada com sucesso!');
           item.verified = true;
+          onOpen();
         } else {
-          alert(
+          setMessageHeader('Erro!');
+          setMessageBody(
             'Ocorreu um erro ao verificar a postagem. Verifique com o administrador',
           );
         }
@@ -54,151 +75,178 @@ const Postagem = ({
         setVerifyingPost(false);
       });
     }
-  }, [verifyingPost, item, onAddSelo]);
+  }, [verifyingPost, item, onAddSelo, onOpen]);
 
   return (
-    <Box
-      p={{base: 4, lg: 6}}
-      mb={6}
-      borderRadius={{base: '0px', lg: '10px'}}
-      shadow="md"
-      bgColor="white">
-      <Stack width="100%">
-        <Flex mb={4} flexDirection="row" align="center">
-          <Box mr={4}>
-            <Avatar
-              name={get(item, 'author.name')}
-              src={get(item, 'author.avatar')}
-            />
-          </Box>
-          <Stack spacing={{base: 0, lg: 1}}>
-            <Text fontWeight="bold" fontSize="sm" color="black">
-              {get(item, 'author.name')}
-            </Text>
-            <Text fontSize="xs" color="gray">
-              {item.dateTime.fromNow()}
-            </Text>
-          </Stack>
-        </Flex>
-        <Stack>
-          <Flex mb={2} flexDirection="row" align="center">
-            <Text fontWeight="bold" size="md" color="black">
-              {item.title}
-            </Text>
-            {(item.verified || verifiable) && (
-              <IconButton
-                aria-label={
-                  item.verified
-                    ? 'Postagem verificada'
-                    : 'Clique aqui para verificar postagem'
-                }
-                cursor={item.verified ? 'auto' : 'pointer'}
-                display="flex"
-                icon={
-                  <Icon
-                    color={!item.verified ? 'gray' : 'green'}
-                    boxSize="1em"
-                    as={MdVerifiedUser}
-                  />
-                }
-                isDisabled={!verifiable}
-                isLoading={verifyingPost}
-                onClick={() =>
-                  verifiable && !item.verified && setVerifyingPost(true)
-                }
-                variant="unstyled"
+    <>
+      <Box
+        p={{base: 4, lg: 6}}
+        mb={6}
+        borderRadius={{base: '0px', lg: '10px'}}
+        shadow="md"
+        bgColor="white">
+        <Stack width="100%">
+          <Flex mb={4} flexDirection="row" align="center">
+            <Box mr={4}>
+              <Avatar
+                name={get(item, 'author.name')}
+                src={get(item, 'author.avatar')}
               />
-            )}
-          </Flex>
-          <Text size="sm" color="black" align="justify">
-            {item.description}
-          </Text>
-          <Text
-            cursor="pointer"
-            fontStyle={openComments ? 'italic' : ''}
-            fontSize="sm"
-            color="gray"
-            align="right"
-            py={2}
-            borderTop="1px solid #eee"
-            borderBottom={openComments ? '1px solid #eee' : ''}
-            onClick={() => setOpenComments(!openComments)}>
-            {item?.comments > 0 ? `${item?.comments} Comentários` : 'Comentar'}
-          </Text>
-          {openComments > 0 && (
-            <Box p={4} px={{base: 0, lg: 4}}>
-              <Flex
-                flexDirection="row"
-                align="center"
-                mb={item?.comments > 0 ? 8 : 0}>
-                <Box mr={{base: 2, lg: 4}}>
-                  <Avatar name={user.name} src={avatar} />
-                </Box>
-
-                <Input
-                  value={newComment}
-                  onChange={(event) => setNewComment(event.target.value)}
-                  borderRadius="50px"
-                  placeholder="Envie um comentário"
-                  size="md"
-                />
-                <mdiSend />
-                <IconButton
-                  disabled={isEmpty(newComment) || isNull(newComment)}
-                  ml={4}
-                  colorScheme="primary"
-                  icon={<Icon fontSize="2xl" as={MdSend} />}
-                  isRound
-                  isLoading={creatingComment}
-                  onClick={(event) => {
-                    setCreatingComment(true);
-                    onCreateComment && onCreateComment(newComment, item.id);
-                  }}
-                />
-              </Flex>
-              <Stack spacing={4}>
-                {loadingComments && (
-                  <Box w="100%" textAlign="center">
-                    <Spinner colorScheme="primary.main" />
-                  </Box>
-                )}
-                {(comments ?? []).map((comment, index) => (
-                  <Flex key={index} flexDirection="row" align="flex-start">
-                    <Box mr={{base: 2, lg: 4}}>
-                      <Avatar
-                        name={comment.author?.name}
-                        src={comment.author?.avatar}
-                      />
-                    </Box>
-                    <Box
-                      p={{base: 3, lg: 4}}
-                      background="#ddd"
-                      borderRadius="10px">
-                      <Stack direction="row" justifyContent="space-between">
-                        <Text
-                          mb={4}
-                          fontWeight="bold"
-                          fontSize="xs"
-                          color="black">
-                          {comment.author?.name}
-                        </Text>
-                        <Text fontSize="xs" color="gray">
-                          {comment.dateTime.fromNow()}
-                        </Text>
-                      </Stack>
-
-                      <Text fontSize="sm" color="black" align="justify">
-                        {comment.body}
-                      </Text>
-                    </Box>
-                  </Flex>
-                ))}
-              </Stack>
             </Box>
-          )}
+            <Stack spacing={{base: 0, lg: 1}}>
+              <Text fontWeight="bold" fontSize="sm" color="black">
+                {get(item, 'author.name')}
+              </Text>
+              <Text fontSize="xs" color="gray">
+                {item.dateTime.fromNow()}
+              </Text>
+            </Stack>
+          </Flex>
+          <Stack>
+            <Flex mb={2} flexDirection="row" align="center">
+              <Text fontWeight="bold" size="md" color="black">
+                {item.title}
+              </Text>
+              {(item.verified || verifiable) && (
+                <IconButton
+                  aria-label={
+                    item.verified
+                      ? 'Postagem verificada'
+                      : 'Clique aqui para verificar postagem'
+                  }
+                  cursor={item.verified ? 'auto' : 'pointer'}
+                  display="flex"
+                  icon={
+                    <Icon
+                      color={!item.verified ? 'gray' : 'green'}
+                      boxSize="1em"
+                      as={MdVerifiedUser}
+                    />
+                  }
+                  isDisabled={!verifiable}
+                  isLoading={verifyingPost}
+                  onClick={() =>
+                    verifiable && !item.verified && setVerifyingPost(true)
+                  }
+                  variant="unstyled"
+                />
+              )}
+            </Flex>
+            <Text size="sm" color="black" align="justify">
+              {item.description}
+            </Text>
+            <Text
+              cursor="pointer"
+              fontStyle={openComments ? 'italic' : ''}
+              fontSize="sm"
+              color="gray"
+              align="right"
+              py={2}
+              borderTop="1px solid #eee"
+              borderBottom={openComments ? '1px solid #eee' : ''}
+              onClick={() => setOpenComments(!openComments)}>
+              {item?.comments > 0
+                ? `${item?.comments} Comentários`
+                : 'Comentar'}
+            </Text>
+            {openComments > 0 && (
+              <Box p={4} px={{base: 0, lg: 4}}>
+                <Flex
+                  flexDirection="row"
+                  align="center"
+                  mb={item?.comments > 0 ? 8 : 0}>
+                  <Box mr={{base: 2, lg: 4}}>
+                    <Avatar name={user.name} src={avatar} />
+                  </Box>
+
+                  <Input
+                    value={newComment}
+                    onChange={(event) => setNewComment(event.target.value)}
+                    borderRadius="50px"
+                    placeholder="Envie um comentário"
+                    size="md"
+                  />
+                  <mdiSend />
+                  <IconButton
+                    disabled={isEmpty(newComment) || isNull(newComment)}
+                    ml={4}
+                    colorScheme="primary"
+                    icon={<Icon fontSize="2xl" as={MdSend} />}
+                    isRound
+                    isLoading={creatingComment}
+                    onClick={(event) => {
+                      setCreatingComment(true);
+                      onCreateComment && onCreateComment(newComment, item.id);
+                    }}
+                  />
+                </Flex>
+                <Stack spacing={4}>
+                  {loadingComments && (
+                    <Box w="100%" textAlign="center">
+                      <Spinner colorScheme="primary.main" />
+                    </Box>
+                  )}
+                  {(comments ?? []).map((comment, index) => (
+                    <Flex key={index} flexDirection="row" align="flex-start">
+                      <Box mr={{base: 2, lg: 4}}>
+                        <Avatar
+                          name={comment.author?.name}
+                          src={comment.author?.avatar}
+                        />
+                      </Box>
+                      <Box
+                        p={{base: 3, lg: 4}}
+                        background="#ddd"
+                        borderRadius="10px">
+                        <Stack direction="row" justifyContent="space-between">
+                          <Text
+                            mb={4}
+                            fontWeight="bold"
+                            fontSize="xs"
+                            color="black">
+                            {comment.author?.name}
+                          </Text>
+                          <Text fontSize="xs" color="gray">
+                            {comment.dateTime.fromNow()}
+                          </Text>
+                        </Stack>
+
+                        <Text fontSize="sm" color="black" align="justify">
+                          {comment.body}
+                        </Text>
+                      </Box>
+                    </Flex>
+                  ))}
+                </Stack>
+              </Box>
+            )}
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
+      </Box>
+
+      {/* Modal de feedback de Mensagem */}
+      <Modal
+        isCentered
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset="slideInBottom">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{messageHeader}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontSize="sm" color="black" align="justify">
+              {messageBody}
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              Fechar
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
