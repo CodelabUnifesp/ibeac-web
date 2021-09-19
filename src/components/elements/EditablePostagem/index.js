cdbd45ac8e2eca8b16d0835857fb8ccf94265ca4
DanielSalis <danielsalis98@gmail.com>
2021-09-19 14:36:33 -0300

    utilização implementação que se aproveita do estado ao invés de onChange

--- src/components/elements/EditablePostagem/index.js
+++ src/components/elements/EditablePostagem/index.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -13,43 +13,56 @@ import {
 import {get} from 'lodash';
 import Textarea from '../Textarea';
 
-const EditablePostagem = ({value, onChange} = {}) => (
-  <div>
-    <FormControl id="title">
-      <FormLabel>Título</FormLabel>
-      <Input
-        colorScheme="primary"
-        type="text"
-        value={get(value, 'title')}
-        onChange={(event) => onChange('title', event.target.value)}
-      />
-      {/* <FormHelperText>Título da sua postagem.</FormHelperText> */}
-    </FormControl>
-    <FormControl id="description">
-      <FormLabel>Descrição</FormLabel>
-      <Textarea
-        colorScheme="primary"
-        value={get(value, 'description')}
-        onChange={(event) => onChange('description', event.target.value)}
-      />
-      {/* <FormHelperText>Título da sua postagem.</FormHelperText> */}
-    </FormControl>
+const EditablePostagem = ({value} = {}) => {
+  return (
+    value && (
+      <div>
+        <FormControl id="title">
+          <FormLabel>Título</FormLabel>
+          <Input
+            colorScheme="primary"
+            type="text"
+            value={value.title}
+            onChange={(event) => {
+              value.title = event.target.value;
+              console.log(value.title);
+            }}
+          />
+          {/* <FormHelperText>Título da sua postagem.</FormHelperText> */}
+        </FormControl>
+        <FormControl id="description">
+          <FormLabel>Descrição</FormLabel>
+          <Textarea
+            colorScheme="primary"
+            value={value.description}
+            onChange={(event) => {
+              value.description = event.target.value;
+              console.log(value.description);
+            }}
+          />
+          {/* <FormHelperText>Título da sua postagem.</FormHelperText> */}
+        </FormControl>
 
-    <FormControl id="category">
-      <FormLabel>Categoria</FormLabel>
-      <Select
-        colorScheme="primary"
-        value={get(value, 'category')}
-        onChange={(event) => onChange('category', event.target.value)}>
-        <option hidden selected />
-        <option value={1}>Saúde</option>
-        <option value={2}>Trocas</option>
-        <option value={3}>Cultura e Lazer</option>
-      </Select>
-      {/* <FormHelperText>Título da sua postagem.</FormHelperText> */}
-    </FormControl>
-  </div>
-);
+        <FormControl id="category">
+          <FormLabel>Categoria</FormLabel>
+          <Select
+            colorScheme="primary"
+            value={value.category}
+            onChange={(event) => {
+              value.category = event.target.value;
+              console.log(value.category);
+            }}>
+            <option hidden selected />
+            <option value={1}>Saúde</option>
+            <option value={2}>Trocas</option>
+            <option value={3}>Cultura e Lazer</option>
+          </Select>
+          {/* <FormHelperText>Título da sua postagem.</FormHelperText> */}
+        </FormControl>
+      </div>
+    )
+  );
+};
 
 EditablePostagem.displayName = 'EditablePostagem';
 EditablePostagem.propTypes = {};
