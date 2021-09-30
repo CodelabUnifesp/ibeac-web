10da2759a90d2471f5fa7c2ffcc3b0091736c0b9
DanielSalis <danielsalis98@gmail.com>
2021-09-30 11:34:47 -0300

    estilizando Stack

---
+++ src/components/elements/Sidebar/styles.js
@@ -0,0 +1,12 @@
+import {Stack} from '@chakra-ui/layout';
+import styled from 'styled-components';
+
+export const StackAside = styled(Stack)`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 25px;
+`;
+
+export default {
+  StackAside,
+};
