1e0bd55329bf95054de99436d0453dff9b79ccaf
DanielSalis <danielsalis98@gmail.com>
2021-09-30 12:18:09 -0300

    adicionando arquivo de media

---
+++ src/media.js
@@ -0,0 +1,16 @@
+import {css} from 'styled-components';
+
+const sizes = {
+  mobile: 1000,
+};
+
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label]}px) {
+      ${css(...args)}
+    }
+  `;
+  return acc;
+}, {});
+
+export default media;
