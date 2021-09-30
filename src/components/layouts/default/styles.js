72c47c8ae2f00c56b8420efbe2400acdc4b48e20
DanielSalis <danielsalis98@gmail.com>
2021-09-30 11:34:12 -0300

    adicionando estilo da barra lateral

--- src/components/layouts/default/styles.js
+++ src/components/layouts/default/styles.js
@@ -12,13 +12,21 @@ export const Content = styled(Box)`
     display: grid;
     grid-template-columns: 300px auto;
     grid-template-rows: auto;
+    grid-template-areas: 'aside .';
     max-width: 1300px;
     margin-left: auto;
     margin-right: auto;
   }
 `;
 
+export const BoxAside = styled(Box)`
+  background: unset;
+  grid-area: aside;
+  padding-bottom: unset;
+`;
+
 export default {
   Container,
   Content,
+  BoxAside,
 };
